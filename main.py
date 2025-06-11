9e5ea32afc288999a9fee215b5a9fc963234f676
Mohsen Moghadam <m.moghadam@hotmail.com>
2025-06-11 22:51:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import telebot
 import requests
 
-TOKEN = "7923818163:AAESxfP1d-QUorPzWJuZExslSPZnxsSVJDM"
+TOKEN = "7117854493:AAFmkXbinHewDsas1j8ZOfQoMTW1dZL4aUA"
 
 bot = telebot.TeleBot(TOKEN)
 bot.set_webhook()
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "سلام! من ربات پایتونی دانیال هستم 🤖")
+    bot.reply_to(message, "سلام جیگر طلا")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    bot.reply_to(message, f"شما گفتید: {message.text}")
-
+    bot.reply_to(message, f"نمره 20 منو بده")
 bot.polling(timeout=200)
